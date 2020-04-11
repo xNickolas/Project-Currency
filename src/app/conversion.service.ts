c9062f2d2d53047e29801cb2a2334a26de7b9b58
diegominelli <diego.minelli@hotmail.com>
2020-04-11 13:30:18 -0300

    criação da segunda função dentro do serviço que consulta a Api

--- src/app/conversion.service.ts
+++ src/app/conversion.service.ts
@@ -6,6 +6,7 @@ import {HttpClient} from '@angular/common/http';
 })
 export class ConversionService {
   private apiUrl = "https://api.exchangeratesapi.io/latest";
+  private historicalMoney = "https://api.exchangeratesapi.io/history?start_at=2018-01-01&end_at=2018-09-01&base=USD/";
 
   base ='?base=';
 
@@ -15,5 +16,9 @@ export class ConversionService {
     let finalurl = this.apiUrl + this.base + moneyBase;
     return this.http.get(finalurl)
   }
+
+  gethistoricalData( /*  TODO: criar parametros da função */) { 
+    // TODO: criar conteudo da função 
+   }
 
 }
